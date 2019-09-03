9a9154adf5e04535e514b9930bc9ec3e11ee2474
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2019-09-03 12:37:37 +0200

    cleaning code

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const tempData = {
-  name:'P',
-  surname:'J'
-}
-
 const TempComponent = ({text})=>{
   return (<h1>{text}</h1>)
 }
 
 const App = () => {
-
-  const {name, surname} = tempData
   return (
     <div className="app">
       <TempComponent text="hello world"/>
